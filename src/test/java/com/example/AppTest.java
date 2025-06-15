9a33e74af7ce011bc9cd669a1262f5b0327cea1e
Nikhil2021WA86075 <2021WA86075@wilp.bits-pilani.ac.in>
2025-06-15 21:32:31 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,12 +1,8 @@
 package com.example;
-
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
 class CodeInspectionApplicationTests {
-
     @Test
     void contextLoads() {
     }
