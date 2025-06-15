d01c148b74de9c53fb0b65abf0f0ad0f7a1c3889
Nikhil2021WA86075 <2021WA86075@wilp.bits-pilani.ac.in>
2025-06-15 21:32:58 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,12 +1,8 @@
 package com.example;
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class CodeInspectionApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CodeInspectionApplication.class, args);
     }
