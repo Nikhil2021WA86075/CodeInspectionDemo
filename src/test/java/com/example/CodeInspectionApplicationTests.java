6440c140da2c5b36da52f3b027b15647e8091996
Nikhil2021WA86075 <2021WA86075@wilp.bits-pilani.ac.in>
2025-06-15 22:22:13 +0530

    Update CodeInspectionApplicationTests.java

--- src/test/java/com/example/CodeInspectionApplicationTests.java
+++ src/test/java/com/example/CodeInspectionApplicationTests.java
@@ -1,11 +1,8 @@
 package com.example;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootApplication
+@SpringBootTest
 class CodeInspectionApplicationTests {
-  
     @Test
     void contextLoads() {
     }
